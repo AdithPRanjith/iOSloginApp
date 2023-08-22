924f1c8437ec8e42fd7572a5e040b016f2d8fbcf
Adith P Ranjith <adith7.1234@gmail.com>
2023-08-22 10:02:35 +0530

    Initial Commit

---
+++ LoginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Adith P Ranjith on 22/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
